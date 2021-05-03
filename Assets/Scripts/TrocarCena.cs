55d7250ea199afe336eb9a6f7b8dbbb9f62e6bef
daniloramin <daniloramin00@hotmail.com>
2021-05-03 19:50:40 -0300

    backup dos arquivos e cenas de UI

---
+++ Assets/Scripts/TrocarCena.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrocarCena : MonoBehaviour
+{
+
+    public string nomeCena;
+
+    public void MudarCena() {
+        SceneManager.LoadScene(nomeCena);
+    }
+
+    public void SairJogo() {
+        Application.Quit();
+    }
+
+}
